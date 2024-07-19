c257b5302e864cc3020be48ad4bbf48db728be33
Samarth Patil <samarthpatil450@gmail.com>
2024-07-19 17:30:26 +0530

    Add Initial Files

--- app.js
+++ app.js
@@ -23,7 +23,6 @@ const user=require('./routes/user.js')
 const passport=require('passport');
 const LocalStrategy=require('passport-local');
 
-// const mongoURL='mongodb://127.0.0.1:27017/wanderlust'
 const dbUrl=process.env.ATLASDB_URL;
 main().then(()=>{
     console.log('connected to db');
